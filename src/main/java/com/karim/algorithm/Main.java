873f62c95b6b13d3a3c19838e34cbf4c586f84a2
수빈 <limsu_@naver.com>
2022-02-07 19:47:52 +0900

    1.  8단계 진행중

--- src/main/java/com/karim/algorithm/Main.java
+++ src/main/java/com/karim/algorithm/Main.java
@@ -8,13 +8,36 @@ public class Main {
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
         try {
-            int n = Integer.parseInt(bufferedReader.readLine());
+            int input = Integer.parseInt(bufferedReader.readLine());
+            int sumResult = 0;
+            int line = 0; // 라인수가 될것이다.
 
-            for (int i=1; i<=n; i++){
-
+            //일단 몇 번째 줄에 있는지 구하기
+            for (int i=1; i < input+1; i++){
+                sumResult = sumResult + i;
+                if (sumResult >= input){
+                    line = i;
+                    break;
+                }
             }
 
-            bufferedWriter.write( "\n");
+            // 그 열 분수 구하기
+            String[] fraction = new String[line];
+            // 쩍수면
+            if (line%2 == 0){
+                for (int i=0; i<line; i++){
+                    fraction[i] = (i+1)+"/"+(line-i);
+                }
+            }else {
+                for (int i=0; i<line; i++){
+                    fraction[i] = (line-i)+"/"+(i+1);
+                }
+            }
+
+            // 해당 번 째 구하기
+            int pick = input - (sumResult - line) - 1;
+
+            bufferedWriter.write( fraction[pick] + "\n");
             bufferedWriter.flush();
 
         } catch (IOException e) {
--- src/main/java/com/karim/algorithm/baekJoon/Step8.java
+++ src/main/java/com/karim/algorithm/baekJoon/Step8.java
@@ -82,11 +82,49 @@ public class Step8 {
     }
 
     /**
-     * NOTE : 	분수찾기
+     * NOTE : 분수찾기
      * => 벌분수의 순서에서 규칙을 찾는 문제
-     * COMMAND :
+     * COMMAND : 너무 어려워 ㅜㅜ
      */
     public void findRule(){
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        try {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            int sumResult = 0;
+            int line = 0; // 라인수가 될것이다.
+
+            //일단 몇 번째 줄에 있는지 구하기
+            for (int i=1; i < input+1; i++){
+                sumResult = sumResult + i;
+                if (sumResult >= input){
+                    line = i;
+                    break;
+                }
+            }
+
+            // 그 열 분수 구하기
+            String[] fraction = new String[line];
+            // 쩍수면
+            if (line%2 == 0){
+                for (int i=0; i<line; i++){
+                    fraction[i] = (i+1)+"/"+(line-i);
+                }
+            }else {
+                for (int i=0; i<line; i++){
+                    fraction[i] = (line-i)+"/"+(i+1);
+                }
+            }
+
+            // 해당 번 째 구하기
+            int pick = input - (sumResult - line) - 1;
+
+            bufferedWriter.write( fraction[pick] + "\n");
+            bufferedWriter.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
